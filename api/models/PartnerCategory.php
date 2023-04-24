455e003a8d30f92bd258c580a7e0d4bf3a210742
T-MA-S <tmas2003@gmail.com>
2023-04-24 20:08:44 +0400

    init new models (+add migrations)

---
+++ api/models/PartnerCategory.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace app\models;
+
+
+/**
+ * This is the model class for table "partner_category".
+ *
+ * @property int $id
+ * @property string|null $category_name
+ *
+ * @property PartnerElement[] $partnerElements
+ */
+class PartnerCategory extends yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'partner_category';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['category_name'], 'required'],
+            [['category_name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPartnerElements()
+    {
+        return $this->hasMany(PartnerElement::class, ['category_id' => 'id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'category_name' => 'Category Name',
+        ];
+    }
+}
---
+++ api/models/PartnerElement.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace app\models;
+
+/**
+ * This is the model class for table "partner_element".
+ *
+ * @property int $id
+ * @property string|null $name
+ * @property string|null $image
+ * @property string|null $url
+ * @property int|null $category_id
+ *
+ * @property PartnerCategory $category
+ */
+class PartnerElement extends yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'partner_element';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'image', 'url', 'category_id'], 'required'],
+            [['name', 'image', 'url'], 'string', 'max' => 255],
+            [['category_id'], 'integer'],
+            [['category_id'], 'exist', 'skipOnError' => true, 'targetClass' => PartnerCategory::class, 'targetAttribute' => ['category_id' => 'id']],
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCategory()
+    {
+        return $this->hasOne(PartnerCategory::class, ['id' => 'category_id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'image' => 'Image',
+            'url' => 'Url',
+            'category_id' => 'Category ID',
+        ];
+    }
+}
